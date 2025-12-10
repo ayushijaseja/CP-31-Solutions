207192744fb180fdd8a9260549611a8d6260cfed
ayushijaseja <ajaseja7@gmail.com>
2025-12-10 17:48:22 +0530

    folder structure corrected

--- 1200 Rated/B_Stone_Age_Problem.cpp
+++ 1200 Rated/B_Stone_Age_Problem.cpp
@@ -19,6 +19,61 @@ using namespace std;
 
 void solve()
 {
+    int n, q;
+
+    cin >> n >> q;
+
+    vector<pair<int, int>> v(n, {0, 0});
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first;
+    }
+    pair<int, int> globalVal = {0, -1};
+
+    long long sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += v[i].first;
+
+    for (int it = 1; it <= q; it++)
+    {
+        int x;
+        cin >> x;
+
+        if (x == 1)
+        {
+            int ind, val;
+            cin >> ind >> val;
+
+            ind--;
+
+            if (v[ind].second > globalVal.second)
+            {
+                sum += (val - v[ind].first);
+            }
+            else
+            {
+
+                sum += (val - globalVal.first);
+            }
+
+            v[ind].first = val;
+            v[ind].second = it;
+        }
+        else
+        {
+            int val;
+
+            cin >> val;
+
+            globalVal.first = val;
+            globalVal.second = it;
+
+            sum = (long long)(val)*n;
+        }
+
+        cout << sum << "\n";
+    }
 }
 
 int32_t main()
@@ -27,7 +82,7 @@ int32_t main()
     cin.tie(0);
     cout.tie(0);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--)
         solve();
     return 0;
