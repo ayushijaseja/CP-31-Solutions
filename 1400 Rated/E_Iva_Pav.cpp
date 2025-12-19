5baa3488d382c9a67542c74a9845af3d5da99f07
ayushijaseja <ajaseja7@gmail.com>
2025-12-19 14:55:19 +0530

    solved 2 today

--- 1400 Rated/E_Iva_Pav.cpp
+++ 1400 Rated/E_Iva_Pav.cpp
@@ -1,63 +1,116 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+#define vin(a)                           \
+    for (int i = 0; i < (a).size(); i++) \
+        cin >> a[i];
+#define vout(a)                          \
+    for (int i = 0; i < (a).size(); i++) \
+        cout << a[i] << ' ';             \
+    cout << endl;
+#define r(x)               \
+    {                      \
+        cout << x << '\n'; \
+        return;            \
+    }
+#define lcm(a, b) ((a) / ([](long long x, long long y) {while(y){long long t=y;y=x%y;x=t;}return x; })(a, b) * (b))
+#define MSB_POS(x) ((x) ? 63 - __builtin_clzll(x) : -1)
+long long modpow(long long a, long long b, long long m)
+{
+    long long r = 1;
+    a %= m;
+    while (b)
+    {
+        if (b & 1)
+            r = r * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return r;
+}
+const int MOD = 1e9 + 7;
+long long fact(long long n)
+{
+    long long r = 1;
+    for (long long i = 1; i <= n; i++)
+        r = (r * i) % MOD;
+    return r;
+}
+bool isSet(int n, int i) { return (n & (1LL << i)) != 0; }
+bool compare(const vector<int> &andOP, const vector<int> &kB)
+{
+    for (int i = 30; i >= 0; i--)
+    {
+        if (andOP[i] != kB[i])
+            return andOP[i] > kB[i];
+    }
+    return true;
+}
+
+bool condition(vector<int> &andOP, int x, vector<int> &kB, vector<vector<int>> &PreC)
+{
+    for (int i = 0; i < 31; i++)
+        if (andOP[i] != PreC[x][i])
+            andOP[i] = 0;
+    return compare(andOP, kB);
+}
 
 void solve()
 {
     int n;
     cin >> n;
     vector<int> v(n);
-    for (auto &x : v)
-        cin >> x;
-
-    // nextZero[i][b] = next index >= i where bit b is 0
-    vector<vector<int>> nextZero(n + 1, vector<int>(31, n));
-    for (int b = 0; b < 31; b++)
-    {
-        nextZero[n][b] = n;
-        for (int i = n - 1; i >= 0; i--)
-        {
-            if ((v[i] & (1LL << b)) == 0)
-                nextZero[i][b] = i;
-            else
-                nextZero[i][b] = nextZero[i + 1][b];
-        }
-    }
-
+    vin(v);
     int q;
     cin >> q;
-    while (q--)
+    vector<pair<int, int>> queries(q);
+    for (int i = 0; i < q; i++)
     {
-        int l, k;
-        cin >> l >> k;
-        l--;
+        int x, y;
+        cin >> x >> y;
+        x--;
+        queries[i] = {x, y};
+    }
 
+    vector<vector<int>> PreC(n, vector<int>(31));
+    for (int i = 0; i < n; i++)
+    {
+        int num = v[i];
+        vector<int> bits(31);
+        for (int i = 0; i < 31; i++)
+            bits[i] = isSet(num, i);
+        PreC[i] = bits;
+    }
+
+    vector<int> ans;
+    for (auto query : queries)
+    {
+        int l = query.first;
+        int k = query.second;
         if (v[l] < k)
         {
-            cout << -1 << " ";
+            ans.push_back(-1);
             continue;
         }
-
-        int r = n - 1;
-        for (int b = 0; b < 31; b++)
-        {
-            if (k & (1LL << b))
-            {
-                r = min(r, nextZero[l][b] - 1);
-            }
-        }
-
-        cout << r + 1 << " ";
+        vector<int> andOP(31, 0);
+        vector<int> kB(31, 0);
+        for (int i = 0; i < 31; i++)
+            kB[i] = isSet(k, i);
+        for (int i = 0; i < 31; i++)
+            andOP[i] = isSet(v[l], i);
+        int r = 0;
+        while (l + r + 1 < n && condition(andOP, l + r + 1, kB, PreC))
+            r++;
+        ans.push_back(l + r + 1);
     }
-    cout << '\n';
+    vout(ans);
 }
 
 int32_t main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
+    cin.tie(0);
+    int t = 1;
     cin >> t;
     while (t--)
         solve();
