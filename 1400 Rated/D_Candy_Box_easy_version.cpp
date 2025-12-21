25b1242e08973356b67474e947a739172f7f5392
ayushijaseja <ajaseja7@gmail.com>
2025-12-21 22:38:05 +0530

    solved 3 today

---
+++ 1400 Rated/D_Candy_Box_easy_version.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vin(a)                           \
+    for (int i = 0; i < (a).size(); i++) \
+        cin >> a[i];
+#define vout(a)                          \
+    for (int i = 0; i < (a).size(); i++) \
+        cout << a[i] << ' ';             \
+    cout << endl;
+#define r(x)               \
+    {                      \
+        cout << x << '\n'; \
+        return;            \
+    }
+#define lcm(a, b) ((a) / ([](long long x, long long y) {while(y){long long t=y;y=x%y;x=t;}return x; })(a, b) * (b))
+#define MSB_POS(x) ((x) ? 63 - __builtin_clzll(x) : -1)
+long long modpow(long long a, long long b, long long m)
+{
+    long long r = 1;
+    a %= m;
+    while (b)
+    {
+        if (b & 1)
+            r = r * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return r;
+}
+const int MOD = 1e9 + 7;
+long long fact(long long n)
+{
+    long long r = 1;
+    for (long long i = 1; i <= n; i++)
+        r = (r * i) % MOD;
+    return r;
+}
+bool isSet(int n, int i) { return (n & (1 << i)) != 0; }
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    vin(v);
+
+    vector<int> typef(n);
+    for (int i = 0; i < n; i++)
+        typef[v[i] - 1]++;
+    sort(typef.begin(), typef.end());
+    int i = n - 1;
+    int ans = 0;
+    while (n != 0 && i >= 0)
+    {
+        // cout << n << " " << typef[i] << endl;
+        if (typef[i] < n)
+            n--;
+        else if (typef[i] >= n)
+        {
+            ans += min(typef[i], n);
+            n--;
+            i--;
+        }
+    }
+    cout << ans << endl;
+    // vout(typef);
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+}
