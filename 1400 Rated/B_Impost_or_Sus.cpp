c5bf3e196413216bf68f5ecb7f7265665c5e1e1f
ayushijaseja <ajaseja7@gmail.com>
2025-12-29 15:48:29 +0530

    contest

---
+++ 1400 Rated/B_Impost_or_Sus.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vin(a)                           \
+    for (int i = 0; i < (a).size(); i++) \
+        cin >> a[i];
+#define vout(a)                          \
+    for (int i = 0; i < (a).size(); i++) \
+        cout << a[i] << ' ';             \
+    cout << endl;
+#define r(x)               \
+    {                      \
+        cout << x << '\n'; \
+        return;            \
+    }
+#define lcm(a, b) ((a) / ([](long long x, long long y) {while(y){long long t=y;y=x%y;x=t;}return x; })(a, b) * (b))
+#define MSB_POS(x) ((x) ? 63 - __builtin_clzll(x) : -1)
+long long modpow(long long a, long long b, long long m)
+{
+    long long r = 1;
+    a %= m;
+    while (b)
+    {
+        if (b & 1)
+            r = r * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return r;
+}
+const int MOD = 1e9 + 7;
+long long fact(long long n)
+{
+    long long r = 1;
+    for (long long i = 1; i <= n; i++)
+        r = (r * i) % MOD;
+    return r;
+}
+bool isSet(int n, int i) { return (n & (1 << i)) != 0; }
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int n = s.length();
+    int op = 0;
+    if (s[0] == 'u')
+    {
+        op++;
+        s[0] = 's';
+    }
+    if (s[n - 1] == 'u')
+    {
+        op++;
+        s[n - 1] = 's';
+    }
+    int curr = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == 'u')
+            curr++;
+        else
+        {
+            op += curr / 2;
+            curr = 0;
+        }
+    }
+
+    r(op);
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+}
